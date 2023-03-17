8c035cf5be1d05906e83ff454758c1aef2140215
George Leyva <leyvageorge@hotmail.com>
2023-03-16 17:13:32 -0700

    New places can not be added yet' git push origin main

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'The One',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Ramen,Pan-Asian',
+    pic: '/images/The One.jpg'
+},{
+    name: 'Asian Kitchen',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Pan-Asian,Tai',
+    pic: '/images/Asian Kitchen.jpg'
+}]
