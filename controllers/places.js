8f2f14408c5ed1e2a130659d600f425c44c08b33
George Leyva <leyvageorge@hotmail.com>
2023-03-18 11:13:53 -0700

    simple changes

--- controllers/places.js
+++ controllers/places.js
@@ -1,9 +1,6 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
-
-
-
 router.get('/',(req,res) => {
     res.render('places/index',{places})
 })
@@ -12,7 +9,7 @@ router.post('/', (req, res) => {
     console.log(req.body)
     if (!req.body.pic) {
       // Default image if one is not provided
-      req.body.pic = 'http://placekitten.com/400/400'
+      req.body.pic = 'http://placepuppy.com/400x400'
     }
     if (!req.body.city) {
       req.body.city = 'Anytown'
@@ -21,7 +18,7 @@ router.post('/', (req, res) => {
       req.body.state = 'USA'
     }
     places.push(req.body)
-    res.redirect('/places')
+    res.redirect('../models/places')
   })
   
 
